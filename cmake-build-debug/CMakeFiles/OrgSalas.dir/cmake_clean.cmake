8272f2152949c3e2d90e2736f28f885415e0e0d3
bmarcosps <brunomarcos@ice.ufjf.br>
2020-09-29 22:40:33 -0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/OrgSalas.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OrgSalas.dir/main.cpp.obj"
+  "OrgSalas.exe"
+  "OrgSalas.exe.manifest"
+  "OrgSalas.pdb"
+  "libOrgSalas.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OrgSalas.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
